e731c37043c5ea9ecc15bbfa564034947ee6668c
liepieshov <liepieshov@gmail.com>
2024-10-09 13:53:46 +0100

    implement single and double linked list with dummies

--- algorithms/linked_list/double_linked/main.py
+++ algorithms/linked_list/double_linked/main.py
@@ -4,7 +4,7 @@ from typing import Any
 
 class Node:
     def __init__(
-        self, value: Any, prev_node: None | Node = None, next_node: None | Node = None
+        self, value: Any, prev_node: Node | None = None, next_node: Node | None = None
     ) -> None:
         self.value = value
         self.prev_node = prev_node
@@ -14,8 +14,11 @@ class Node:
 class DoubleLinkedList:
     def __init__(self) -> None:
         self.__length = 0
-        self.__head: None | Node = None
-        self.__tail: None | Node = None
+        self.__head = Node(
+            None,
+        )
+        self.__tail = Node(None, prev_node=self.__head)
+        self.__head.next_node = self.__tail
 
     def __len__(self) -> int:
         return self.__length
@@ -23,24 +26,22 @@ class DoubleLinkedList:
     def __remove_node(self, node: Node) -> None:
         self.__length -= 1
         prev = node.prev_node
+        assert prev is not None
         _next = node.next_node
-        if prev is not None:
-            prev.next_node = _next
-        else:
-            self.__head = _next
-        if _next is not None:
-            _next.prev_node = prev
-        else:
-            self.__tail = prev
+        assert _next is not None
         node.prev_node = None
         node.next_node = None
+        prev.next_node = _next
+        _next.prev_node = prev
 
     def remove(self, value: Any) -> None:
         # removes first occurance of value
-        node = self.__head
-        while node and node.value != value:
+        node = self.__head.next_node
+        assert node is not None
+        while node != self.__tail and node.value != value:
             node = node.next_node
-        if node is not None:
+            assert node is not None
+        if node != self.__tail:
             self.__remove_node(node)
 
     def remove_at(self, idx: int) -> None:
@@ -48,25 +49,15 @@ class DoubleLinkedList:
         self.__remove_node(node)
 
     def append(self, value: Any) -> None:
-        new_node = Node(value)
-        if self.__length == 0:
-            self.__head = new_node
-        else:
-            assert self.__tail is not None
-            self.__tail.next_node = new_node
-            new_node.prev_node = self.__tail
-        self.__tail = new_node
+        self.__tail.prev_node = Node(
+            value, prev_node=self.__tail.prev_node, next_node=self.__tail
+        )
         self.__length += 1
 
     def prepend(self, value: Any) -> None:
-        new_node = Node(value)
-        if self.__length == 0:
-            self.__tail = new_node
-        else:
-            assert self.__head is not None
-            self.__head.prev_node = new_node
-            new_node.next_node = self.__head
-        self.__head = new_node
+        self.__head.next_node = Node(
+            value, prev_node=self.__head, next_node=self.__head.next_node
+        )
         self.__length += 1
 
     def __get_node(self, idx: int) -> Node:
@@ -76,12 +67,12 @@ class DoubleLinkedList:
         jumps_left = idx
         jumps_right = self.__length - 1 - idx
         if jumps_left < jumps_right:
-            node = self.__head
+            node = self.__head.next_node
             for _ in range(jumps_left):
                 assert node is not None
                 node = node.next_node
         else:
-            node = self.__tail
+            node = self.__tail.prev_node
             for _ in range(jumps_right):
                 assert node is not None
                 node = node.prev_node
@@ -99,14 +90,11 @@ class DoubleLinkedList:
         if idx == 0:
             self.prepend(value)
             return
-        insert_after = idx - 1
-        node = self.__get_node(insert_after)
-        # save ref
-        node_next = node.next_node
 
-        new_node = Node(value, prev_node=node, next_node=node_next)
-        node.next_node = new_node
-        if node_next is None:
-            self.__tail = new_node
-        else:
-            node_next.prev_node = new_node
+        insert_after = idx - 1
+        parent_node = self.__get_node(insert_after)
+        cur_next = parent_node.next_node
+        assert cur_next is not None
+        new_node = Node(value, prev_node=parent_node, next_node=cur_next)
+        parent_node.next_node = new_node
+        cur_next.prev_node = new_node
--- algorithms/linked_list/single_linked/main.py
+++ algorithms/linked_list/single_linked/main.py
@@ -6,3 +6,67 @@ class Node:
     def __init__(self, value: Any, next_node: None | Node = None) -> None:
         self.value = value
         self.next_node = next_node
+
+
+class SingleLinkedList:
+    def __init__(self) -> None:
+        self.__length = 0
+        self.__head = Node(None)
+
+    def __len__(self) -> int:
+        return self.__length
+
+    def __remove_next_node(self, parent_node: Node) -> None:
+        self.__length -= 1
+        assert parent_node.next_node is not None
+        removed_node = parent_node.next_node
+        parent_node.next_node = removed_node.next_node
+        removed_node.next_node = None
+
+    def remove(self, value: Any) -> None:
+        # removes first occurance of value
+        node = self.__head
+        while node.next_node and node.next_node.value != value:
+            node = node.next_node
+        if node.next_node is not None:
+            self.__remove_next_node(node)
+
+    def remove_at(self, idx: int) -> None:
+        parent_node = self.__get_parent_node(idx)
+        self.__remove_next_node(parent_node)
+
+    def prepend(self, value: Any) -> None:
+        cur_next = self.__head.next_node
+        new_node = Node(value, next_node=cur_next)
+        self.__head.next_node = new_node
+        self.__length += 1
+
+    def __get_parent_node(self, idx: int) -> Node:
+        if self.__length <= idx:
+            raise IndexError(f"{idx} out of bound {self.__length}")
+        node = self.__head
+        for _ in range(idx):
+            assert node is not None
+            node = node.next_node
+        assert node is not None
+        return node
+
+    def __getitem__(self, idx: int) -> Any:
+        parent_node = self.__get_parent_node(idx)
+        node = parent_node.next_node
+        assert node is not None
+        return node.value
+
+    get = __getitem__
+
+    def insert_at(self, value: Any, idx: int) -> None:
+        # insert before first
+        idx = min(self.__length, idx)
+        if idx == 0:
+            self.prepend(value)
+            return
+        insert_after = idx - 1
+        parent_node = self.__get_parent_node(insert_after).next_node
+        assert parent_node is not None
+        parent_node.next_node = Node(value, next_node=parent_node.next_node)
+        self.__length += 1
